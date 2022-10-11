d7d5e5fd9c428912281f108921696301ed97d80f
Rhutwik jaiswal <103762165+rhutwikjaiswal@users.noreply.github.com>
2022-10-11 22:00:04 +0530

    new

--- script.js
+++ script.js
@@ -1,47 +1,38 @@
-const city = document.getElementById("city")
-const search = document.getElementById("search")
-
+const city = document.getElementById("text");
+const button = document.getElementById("button");
 
 async function fetchWeather(cityName) {
+  let apiKey = "fd58563d7e66df546d92a647b21108e5";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-    let apiKey = "fd58563d7e66df546d92a647b21108e5"
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+  try {
+    let response = await fetch(api);
+    let result = await response.json();
+    console.log(result);
+ 
+    let Orgtemp = Math.trunc(result.main.temp - 273);
+    document.getElementById("temp").innerText = Orgtemp+" ºC";
+    document.getElementById("cit").innerText = " Weather in "+result.name;
+    document.getElementById("weather").innerText = result.weather[0].main;
+    document.getElementById("humidity").innerText = "Humidity: "+result.main.humidity+"%";
+    document.getElementById("wind").innerText ="Wind Speed: "+ result.wind.speed+" km/hr";
 
-    try {
-        let response = await fetch(api)
-        let result = await response.json()
-        document.getElementById("temp").innerText = result.main.temp
-        document.getElementById("maxTemp").innerText = result.main.temp_max
-        document.getElementById("minTemp").innerText = result.main.temp_min
-        document.getElementById("humidity").innerText = result.main.humidity
-
-    }
-    catch (error) {
-        console.log(error)
-    }
-
+    // if(  result.weather[0].main == "rain"){
+    //   document.body.style.backgroundImage = "url('https://images.pexels.com/photos/1100946/pexels-photo-1100946.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+    //   console.log(result.weather[0].main);
+    // }
+ 
+  } catch (error) {
+    console.log(error);
+  }
 
 
 }
 
-
-// on click  on search button(adding event listner)
-search.addEventListener("click", (e) => {
-
-    // grab input from city input field  
-    let cityName = city.value
-
-    // ask weather app  to give me weather data of that city
-     fetchWeather(cityName)
-
-    // fetch(api)
-    // .then((response)=>{
-    //  return response.json()
-    // })
-    // .then((response)=>{
-    //     console.log(response)
-    //    })
-    // .catch((error)=>{
-    //     console.log(error)
-    // })
-})
+button.addEventListener("click", (e) => {
+  let cityName = city.value;
+  fetchWeather(cityName);
+  if(city.value = " "){
+    
+  };
+});
